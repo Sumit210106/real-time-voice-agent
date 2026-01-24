885a2f12876a9749d15f19323cbcc40d2b11dc41
sumit Nayak <sumitnayak2006@gmail.com>
2026-01-24 13:07:42 +0530

    feat: noise suppression logic

--- backend/app/ws.py
+++ backend/app/ws.py
@@ -67,19 +67,69 @@ async def websocket_handler(websocket :WebSocket):
     except WebSocketDisconnect: 
         remove_sessions(session_id)
         print(f"[{session_id}] Disconnected")
+      
+      
         
+class NoiseHero:
+    def __init__(self, alpha:float = 0.95, floor:float = 0.1):
+        self.alpha = alpha
+        self.floor = floor
+        self.noise_memory = None
+        self.window = np.hamming(1024)
+    
+    def suppress(self, raw: np.ndarray) -> np.ndarray:
+        if len(raw) != len(self.window):
+            self.window = np.hamming(len(raw))
+
+
+        # domain frequency
+        fft_data = np.fft.rfft(raw * self.window)
+
+        magnitude = np.abs(fft_data)
+        phase = np.angle(fft_data)
+        
+        if self.noise_memory is None:
+            self.noise_memory = magnitude.copy()
+            return raw
+        
+        # gain
+        snr = magnitude / (self.noise_memory + 1e-6)
+        gain = snr / (snr + 1.0)
+        
+        # spectral floor
+        gain = np.maximum(gain, self.floor)
+        
+        # update noise memory
+        if np.mean(magnitude) < 0.05:
+            self.noise_memory = self.alpha * self.noise_memory + (1 - self.alpha) * magnitude   
+        
+        # Reconstruct
+        clean_mag = magnitude * gain
+        clean_fft = clean_mag * np.exp(1j * phase)
+        clean = np.fft.irfft(clean_fft, n=len(raw))
+        
+        return np.clip(clean, -1.0, 1.0).astype(np.float32)
+    
+    
+
+    
 # binary audio ws
 async def audio_ws(websocket: WebSocket):
     await websocket.accept()
     print("Audio WS connected")
 
+    noise_hero = NoiseHero()
+    
     try:
         while True:
             data = await websocket.receive_bytes()
             samples = np.frombuffer(data,dtype = np.float32)
             # print(f"samples -> {samples}")
-            rms = np.sqrt(np.mean(samples**2))
-            print(f"Chunk received: {len(samples)} samples | RMS: {rms:.5f}")
+            clean = noise_hero.suppress(samples)
+            # print(clean)
+            # print(f"clean -> {clean}")
+            rms = np.sqrt(np.mean(clean**2))
+            print(f"Chunk received: {len(clean)} samples | RMS: {rms:.5f}")
             
     except WebSocketDisconnect:
         print("Audio WS disconnected")
